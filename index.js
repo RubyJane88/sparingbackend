7cb909db9b68628dd4064e3aeb8a0a4a7febfc1e
ruby jane <rubyjanecabagnot@Rubys-MacBook-Pro.local>
2024-02-05 22:40:17 +0100

    Update CORS configuration

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 require("dotenv").config();
 
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 const port = process.env.PORT || 4000;
-console.log("Port is registered..");
+
 const errorHandler = require("./src/middleware/errorHandler.middleware");
-console.log("Error handler middleware is registered");
 const indexRouter = require("./src/api/route/index.route");
-console.log("Index router is registered");
+const allowedOrigins = [
+  process.env.CORS_ORIGIN_1,
+  process.env.CORS_ORIGIN_2
+];
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(errorHandler);
-console.log("Error handler middleware is registered");
 
-app.use(cors({ origin: "*" }));
-console.log("Cors is registered");
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true 
+}));
+
+
 app.use(indexRouter);
-console.log("Index router is registered");
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
