51838e1ce0150cd672eaee343fe48d1afff50ad5
ruby jane <rubyjanecabagnot@Rubys-MacBook-Pro.local>
2024-02-06 01:13:49 +0100

    update cors

--- index.js
+++ index.js
@@ -8,29 +8,26 @@ const port = process.env.PORT || 4000;
 
 const errorHandler = require("./src/middleware/errorHandler.middleware");
 const indexRouter = require("./src/api/route/index.route");
-const allowedOrigins = [
-  process.env.CORS_ORIGIN_1,
-  process.env.CORS_ORIGIN_2
-];
+const allowedOrigins = [process.env.CORS_ORIGIN_1, process.env.CORS_ORIGIN_2];
 
+const cleanOrigins = allowedOrigins.map((origin) => origin.replace(/\/$/, ""));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true 
-}));
-
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || cleanOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Blocked origin:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(indexRouter);
 
